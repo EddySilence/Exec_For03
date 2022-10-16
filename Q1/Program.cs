4cdcdb83431cdb055c41a16a1693a885f107dd9b
eddy <eddy19991118@gmail.com>
2022-10-16 23:23:09 +0800

    加入專案檔案。

---
+++ Q1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //題目：右側對齊星形三角形
+
+            int length = 5;//長度
+
+            for (int i = 1; i <= length; i++)
+            {
+                Console.WriteLine(new String(' ', length - i) + new String('*', i));
+            }
+
+        }
+    }
+}
